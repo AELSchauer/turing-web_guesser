49cc8d58356ea30eee285fd83652a08e3ecf5553
Ashley Schauer <aelschauer@gmail.com>
2017-01-18 20:01:33 -0700

    I4: adds extensions for guess limit and cheat mode

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,25 +1,62 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-SECRET_NUMBER = rand(101)
+guesser = WebGuesser.new
 
 get '/' do
-  guess = params["guess"].to_i
-  user_message, number_message = check_guess(guess)
-  erb :index, :locals => {:user_message => user_message, :number_message => number_message}
+  cheat = true?(params["cheat"])
+  guess = params["guess"]
+  if guesser.guess_tries == 0
+    guesser = WebGuesser.new
+  end
+  user_message, tries_message = guesser.check_guess(guess)
+  secret_number_message = guesser.cheat_mode_on?(cheat)
+  erb :index, :locals => {
+    :user_message => user_message,
+    :secret_number_message => secret_number_message,
+    :tries_message => tries_message
+  }
 end
 
-def check_guess(guess)
-  user_message = ""
-  number_message = "The secret number is #{SECRET_NUMBER}."
-  diff = guess - SECRET_NUMBER
-  if diff == 0
-    user_message = "You got it right!"
-    number_message = ""
-  else
-    way = diff.abs >= 5 ? "way " : ""
-    highlow = diff.abs == diff ? "high" : "low"
-    user_message = "#{way}too #{highlow}!".capitalize
+class WebGuesser
+
+  attr_reader :guess_tries
+
+  def initialize
+    @secret_number = rand(101)
+    @guess_tries = 5
   end
-  [user_message, number_message]
+
+  def check_guess(guess)
+    if guess.nil? || guess.empty?
+      user_message = ""
+      tries_message = "#{@guess_tries} tries left."
+    else
+      diff = guess.to_i - @secret_number
+      @guess_tries -= 1
+      tries_message = ""
+      if diff == 0
+        user_message = "You got it right in #{5-@guess_tries} tries!"
+        @guess_tries = 0
+      elsif @guess_tries == 0
+        user_message = "Sorry, you lose! Enter a new number to try again."
+        @guess_tries = 0
+      else
+        extent = diff.abs >= 5 ? "way too" : "too"
+        highlow = diff.abs == diff ? "high" : "low"
+        user_message = "#{extent} #{highlow}!".capitalize
+        tries_message = "#{@guess_tries} tries left."
+      end
+    end
+    [user_message, tries_message]
+  end
+
+  def cheat_mode_on?(cheat)
+    cheat ? "The secret number is #{@secret_number}." : ""
+  end
+
+end
+
+def true?(statement)
+  statement == "true"
 end
